2d02003a1e23a0fb8a5532bca760e0bf0317f770
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-05-24 16:18:52 +0900

    start game

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,52 @@
 var startButtonEl = document.querySelector(".startButton");
 var viewHighscoresEl = document.querySelector(".viewHighscores");
 var timerEl = document.querySelector(".timerCount");
+var choiceOne, choiceTwo, choiceThree, choiceFour;
 
 var codingQuizEl = document.querySelector(".codingQuiz");
 var questionEl = document.querySelector(".question");
 var choicesEl = document.querySelector(".choices");
 var answerEl = document.querySelector(".answer");
-var correct = false;
-var timer;
+
+var timer = 90;
 var timerCount;
+var score = 0;
+var choiceOne, choiceTwo, choiceThree, choiceFour;
+var questionCounter = 0;
+
 
 let questionList = [
     {
-    "question": "", //q1
+    "question": "Which symbol is used to call an id from the HTML?", //q1
     "choices": {
-        a:"",
-        b:"",
-        c:"",
-        d:"",
+        a:"#",
+        b:".",
+        c:"/",
+        d:"*",
         },
-    "answer": "",
+    "answer": "a",
     },
 
     {
-    "question": "", //q2
+    "question": "Which of the following brackets are required for functions()?", //q2
     "choices": {
-        a:"",
-        b:"",
-        c:"",
-        d:"",
+        a:"{}",
+        b:"[]",
+        c:"<>",
+        d:"||",
         },
-    "answer": "",
+    "answer": "a",
     },
 
     {
-    "question": "", //q3
+    "question": "Which of the following languages is most commonly associated with 'styling' the webpage?", //q3
     "choices": {
-        a:"",
-        b:"",
-        c:"",
-        d:"",
+        a:"HTML",
+        b:"Javascript",
+        c:"Css",
+        d:"JQuery",
         },
-    "answer": "",
+    "answer": "c",
     },
 
     {
@@ -76,29 +81,37 @@ let questionList = [
     },
 ]
 
-function startGame() {
-    isWin = false;
-    timerCount = 70;
-    startTimer();
+
+function resetTimer(){
+    timerCount = 0;
 }
 
 function startTimer() {
     timer = setInterval(function() {
-        if (timerCount > 1) {
+        if (timerCount >= 1) {
             timerCount--;
             timerEl.textContent = timerCount + " seconds remaining";
-        }if (timerCount === 1){
-            timerCount--;
-            timerEl.textContent = timerCount + " second remaining";
-          
-        }else {
-            clearInterval(timer);
-            timerCount = 0;
-            timerEl.textContent = "Time is up!"
-            endQuiz();
+            }else if (timerCount === 1){
+                timerCount--;
+                timerEl.textContent = timerCount + " second remaining";
+            
+            } else if (timerCount === 0) {
+                clearInterval(timer)
+                resetTimer();
+            }
+            else {
+                clearInterval(timer);
+                timerCount = 0;
+                timerEl.textContent = "Time is up!"
+                resetTimer();
+            }
+            }, 1000);
         }
-        }, 1000);
-    }
+
+}
+function homeScreen() {
+
+}
 
 function timeManager() {
     if (questionList.answer=true) {
@@ -108,11 +121,106 @@ function timeManager() {
         score -= 1;
         timer-= 5;
     }
-
-
-function endQuiz() {
-    timer = 0
 }
 
+function startQuiz() {
+    startTimer();
+    clearInterval(timer);
+    if (questionCounter === 0) {
+        choicesEl.innerHTML = "";
+        createButton(questionList[questionCounter]);
+        createListeners();
+    } else if (questionCounter === 1) {
+        choicesEl.innerHTML = "";
+        createButton(questionList[questionCounter]);
+        createListeners();
+    } else if (questionCounter === 2) {
+        choicesEl.innerHTML = "";
+        createButton(questionList[questionCounter]);
+        createListeners();
+    } else if (questionCounter === 3) {
+        choicesEl.innerHTML = "";
+        createButton(questionList[questionCounter]);
+        createListeners();
+    } else if (questionCounter === 4) {
+        choicesEl.innerHTML = "";
+        createButton(questionList[questionCounter]);
+        createListeners();
+    } else if (questionCounter === 5) {
+        choicesEl.innerHTML = "";
+        createButton(questionList[questionCounter]);
+        createListeners();
+    } else if (questionCounter === 6) {
+        choicesEl.innerHTML = "";
+        createButton(questionList[questionCounter]);
+        createListeners();
+    } else {
+        endGame();
+    }
+}
+function endGame() {
 
+createQuestion(questionObj){
+    questionEl.innerHTML = questionObj.question
+}
+
+var createButton = document.createElement("button");
+function createButton(obj){
+    var answerOne = document.createElement("button");
+    answerOne.textContent = "a) " +obj.answers["a"]
+    answerOne.setAttribute("id", "a")
+    choicesEl.appendChild(answerOne)
+    firstChoice = choicesEl.querySelector("#a");
+
+    var answerTwo = document.createElement("button");
+    answerTwo.textContent = "a) " + obj.answers["a"]
+    answerTwo.setAttribute("id", "a")
+    choicesEl.appendChild(answerTwo)
+    secondChoice = choicesEl.querySelector("#a");
+
+    var answerThree = document.createElement("button");
+    answerThree.textContent = "c) " + obj.answers["c"]
+    answerThree.setAttribute("id", "c")
+    choicesEl.appendChild(answerThree)
+    thirdChoice = choicesEl.querySelector("#a");
+
+    var answerFour = document.createElement("button");
+    answerFour.textContent = "a) " + obj.answers["a"]
+    answerFour.setAttribute("id", "a")
+    choicesEl.appendChild(answerFour)
+    fourthChoice = choicesEl.querySelector("#a");
+
+    var answerFive = document.createElement("button");
+    answerFive.textContent = "a) " + obj.answers["a"]
+    answerFive.setAttribute("id", "a")
+    choicesEl.appendChild(answerFive)
+    fifthChoice = choicesEl.querySelector("#a");
+
+    var answerSix = document.createElement("button");
+    answerSix.textContent = "a) " + obj.answers["a"]
+    answerSix.setAttribute("id", "a")
+    choicesEl.appendChild(answerSix)
+    sixthChoice = choicesEl.querySelector("#a");
+}
+
+createListeners(){
+    firstChoice.addEventListener("click", choiceOneSelected)
+    secondChoice.addEventListener("click", choiceTwoSelected)
+    thirdChoice.addEventListener("click", choiceThreeSelected)
+    fourthChoice.addEventListener("click", choiceFourSelected)
+}
+
+function choiceOneSelected() {
+    if ("a" === questionList[questionCounter].correctAnswer)
+        setTimeout(() => {
+            answerEl.innerHTML = "";
+            questionCounter++;
+            score += 5;
+            startGame();
+        }, 1000);
+        else {
+            answerEl.innterHTML = ""
+        }
+        answerEl.innerHTML = "Correct"
+}
 startButtonEl.addEventListener("click", startGame)
